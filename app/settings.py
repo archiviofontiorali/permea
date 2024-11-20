39475af6f58e68609aab52cad4e3124579ff2a04
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2024-11-20 18:46:48 +0100

    Move to SPARQL

--- app/settings.py
+++ app/settings.py
@@ -5,4 +5,4 @@ from decouple import config
 PROJECT_FOLDER = Path(__file__).parent.parent
 HIDDEN_DATA_FOLDER = PROJECT_FOLDER / ".data"
 
-BERKELEY_DB_PATH = config("BERKELEY_DB_PATH", HIDDEN_DATA_FOLDER / "berkeley.db", Path)
+SPARQL_ENDPOINT = config("SPARQL_ENDPOINT", "http://localhost:7878")
