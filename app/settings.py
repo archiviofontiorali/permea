bb585f300a2519a5635a22738aa30c543c033e8a
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2025-02-07 02:21:11 +0100

    Add settings for NAMESPACE and SPARQL authentication

--- app/settings.py
+++ app/settings.py
@@ -6,4 +6,17 @@ PROJECT_FOLDER = Path(__file__).parent.parent
 HIDDEN_DATA_FOLDER = PROJECT_FOLDER / ".data"
 
 SPARQL_ENDPOINT = config("SPARQL_ENDPOINT", "http://localhost:7878")
+SPARQL_USERNAME = config("SPARQL_USERNAME", "admin")
+SPARQL_PASSWORD = config("SPARQL_PASSWORD", "changeme")
+
 SPARQL_DEFAULT_IDENTIFIER = config("SPARQL_DEFAULT_IDENTIFIER", "default")
+
+
+BASE_URL = "http://localhost/"
+
+NAMESPACES = {
+    "": BASE_URL,
+    "ex": "http://example.org/",
+    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+    "foaf": "http://xmlns.com/foaf/0.1/",
+}
