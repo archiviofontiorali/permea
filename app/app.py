dbbac63b0c5a69167706fbcba81c706211ea04cd
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2025-02-07 14:58:34 +0100

    Add class Container

--- app/app.py
+++ app/app.py
@@ -1,6 +1,14 @@
 """Define the main application"""
 
 
+class Container[T]:
+    def __init__(self, **kwargs: T):
+        self.__dict__.update(kwargs)
+
+    def __getattr__(self, item: str) -> T:
+        return self.__dict__[item]
+
+
 class App:
     def __init__(self):
         pass
