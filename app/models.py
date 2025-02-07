60e3d5a3ef7ee256e64202acb07f90c42a2cf96e
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2025-02-07 02:26:20 +0100

    Add new models and repository

--- app/models.py
+++ app/models.py
@@ -1,18 +1,66 @@
-from typing import Union
+from dataclasses import dataclass
+from typing import Annotated
 
-from pydantic import BaseModel, HttpUrl
+from loguru import logger
+from pydantic import BaseModel
 
-Identifier = Union[HttpUrl, str]
-
-
-class Triplet(BaseModel):
-    subject: str
-    predicate: str
-    object: str
+URI = str
 
 
-class Resource(BaseModel):
-    id: Identifier
-    type: HttpUrl | str
+class Model(BaseModel):
+    pass
 
-    # triples: list[Triplet]
+
+class Resource(Model):
+    uri: URI
+
+    def get_property_uri(self, field_name: str) -> URI:
+        try:
+            return self.model_fields.get(field_name).metadata[0].uri
+        except IndexError:
+            logger.debug(self.model_fields.get(field_name))
+            raise Exception(f"Missing annotation in field {field_name}")
+
+    def as_triplets(self):
+        return [
+            (self.uri, self.get_property_uri(field), getattr(self, field))
+            for field, value in self
+            if field not in {"uri"}
+        ]
+
+
+# @dataclass
+# class NamespacedURI:
+#     ns: str
+#     value: str
+#
+#     @property
+#     def uri(self):
+#         return f"{settings.NAMESPACES.get(self.ns)}{self.value}"
+
+
+@dataclass
+class Property:
+    uri: URI
+
+
+class Person(Resource):
+    first_name: Annotated[str, Property(uri="foaf:firstName")]
+    last_name: Annotated[str, Property(uri="foaf:lastName")]
+
+
+# NAMESPACE_RE = re.compile(r"^(?P<ns>\w+):(?P<reference>.*)")
+#
+#
+# def expand_prefix(value: HttpUrl | str) -> HttpUrl | str:
+#     if not isinstance(value, str) or (match := NAMESPACE_RE.match(value)) is None:
+#         return value
+#
+#     if (namespace := match.group("ns")) in {"http", "https"}:
+#         return value
+#
+#     if (prefix := settings.NAMESPACES.get(namespace)) is None:
+#         raise ValueError(f"Unsupported namespace '{namespace}'")
+#
+#     return f"{prefix}{match.group('reference')}"
+#
---
+++ app/repositories.py
@@ -0,0 +1,58 @@
+import abc
+from functools import cache, cached_property
+
+from loguru import logger
+
+from . import settings
+from .models import Person, Resource
+
+
+class PersonRepository(abc.ABC):
+    def create_person(self, person: Person, **additional_fields):
+        pass
+
+    def get_person(self, person_id: int) -> Person:
+        pass
+
+
+class SPARQLRepository(PersonRepository):
+    def __init__(self, sparql_endpoint: str = settings.SPARQL_ENDPOINT):
+        from SPARQLWrapper import DIGEST, JSON, SPARQLWrapper
+
+        self.sparql = SPARQLWrapper(sparql_endpoint)
+        self.sparql.setHTTPAuth(DIGEST)
+        self.sparql.setCredentials(settings.SPARQL_USERNAME, settings.SPARQL_PASSWORD)
+        self.sparql.setReturnFormat(JSON)
+
+    @cache
+    def build_prefixes(self):
+        return "\n".join(
+            f"PREFIX {ns}: <{url}>" for ns, url in settings.NAMESPACES.items()
+        )
+
+    def create_person(self, person: Person, **additional_fields):
+        import json
+
+        from SPARQLWrapper import POST
+
+        query = self.build_prefixes()
+        query += "\n\nINSERT DATA {\n"
+
+        for s, p, o in person.as_triplets():
+            query += f'\t{s} {p} "{o}" .\n'
+
+        query += "}"
+
+        logger.debug(f"SPARQL query:\n{query}")
+
+        self.sparql.setMethod(POST)
+        self.sparql.setQuery(query)
+        response = self.sparql.query().response
+        response = json.loads(response.read().decode("utf-8"))
+        return response
+
+
+if __name__ == "__main__":
+    repo = SPARQLRepository()
+    person_ = Person(uri=":tizio", first_name="Caio", last_name="Sempronio")
+    print(repo.create_person(person_))
