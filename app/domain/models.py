5658ae39a478f12608a3e95bc1003dcbc81f14bd
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2025-02-08 01:48:51 +0100

    Recreate models to use rdflib URIRef

--- app/domain/models.py
+++ app/domain/models.py
@@ -1,56 +1,29 @@
-from dataclasses import dataclass
 from typing import Annotated
 
-from loguru import logger
 from pydantic import BaseModel
+from app import settings
 
-URI = str
+from rdflib import URIRef as URI
+from rdflib.namespace import FOAF, RDF
 
+from pydantic import ConfigDict
 
-class Model(BaseModel):
-    pass
-
-
-class Resource(Model):
-    uri: URI
-
-    def get_property_uri(self, field_name: str) -> URI:
-        try:
-            return self.model_fields.get(field_name).metadata[0].uri
-        except IndexError:
-            logger.debug(self.model_fields.get(field_name))
-            raise Exception(f"Missing annotation in field {field_name}")
-
-    def as_triplets(self):
-        return [
-            (self.uri, self.get_property_uri(field), getattr(self, field))
-            for field, value in self
-            if field not in {"uri"}
-        ]
+ResourceID = str
+URIType = Annotated[URI, RDF.type]
 
 
-@dataclass
-class Property:
-    uri: URI
+class Resource(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    id: ResourceID
+    type: URIType
+
+    @property
+    def uri(self):
+        return settings.NAMESPACE[self.id]
 
 
 class Person(Resource):
-    first_name: Annotated[str, Property(uri="foaf:firstName")]
-    last_name: Annotated[str, Property(uri="foaf:lastName")]
-
-
-# NAMESPACE_RE = re.compile(r"^(?P<ns>\w+):(?P<reference>.*)")
-#
-#
-# def expand_prefix(value: HttpUrl | str) -> HttpUrl | str:
-#     if not isinstance(value, str) or (match := NAMESPACE_RE.match(value)) is None:
-#         return value
-#
-#     if (namespace := match.group("ns")) in {"http", "https"}:
-#         return value
-#
-#     if (prefix := settings.NAMESPACES.get(namespace)) is None:
-#         raise ValueError(f"Unsupported namespace '{namespace}'")
-#
-#     return f"{prefix}{match.group('reference')}"
-#
+    type: URIType = FOAF.Person
+    first_name: Annotated[str, FOAF.firstName]
+    last_name: Annotated[str, FOAF.lastName]
