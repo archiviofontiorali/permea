2d62a592913b6c6de9e0cdfe41d51c45e914c188
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2025-02-08 01:50:22 +0100

    Add Mock Repository

--- app/infrastructure/repositories.py
+++ app/infrastructure/repositories.py
@@ -1,50 +1,49 @@
 from functools import cache
 
-from loguru import logger
-
 from app import settings
-from app.domain.models import Person
+from app.domain.models import Person, ResourceID
 from app.domain.repos import PersonRepository
+from app.infrastructure.services import SPARQLService
+
+
+class MockRepository(PersonRepository):
+    def __init__(self):
+        _data = [
+            Person(id="tizio-caio", first_name="Tizio", last_name="Caio"),
+            Person(id="dr-who", first_name="John", last_name="Smith"),
+        ]
+        self.data = {value.id: value for value in _data}
+
+    def create_person(self, person):
+        if person.uri in self.data:
+            raise Exception("Person already exists")
+        self.data[person.uri] = person
+
+    def retrieve_person(self, person_id) -> Person:
+        return self.data[person_id]
 
 
 class SPARQLRepository(PersonRepository):
-    def __init__(self, sparql_endpoint: str = settings.SPARQL_ENDPOINT):
-        from SPARQLWrapper import DIGEST, JSON, SPARQLWrapper
-
-        self.sparql = SPARQLWrapper(sparql_endpoint)
-        self.sparql.setHTTPAuth(DIGEST)
-        self.sparql.setCredentials(settings.SPARQL_USERNAME, settings.SPARQL_PASSWORD)
-        self.sparql.setReturnFormat(JSON)
+    def __init__(self, service: SPARQLService):
+        self.service = service
 
     @cache
-    def build_prefixes(self):
+    def _build_prefixes(self):
         return "\n".join(
             f"PREFIX {ns}: <{url}>" for ns, url in settings.NAMESPACES.items()
         )
 
-    def create_person(self, person: Person, **additional_fields):
-        import json
+    def create_person(self, person: Person):
+        query = f"""
+        {self._build_prefixes()}
+        INSERT DATA {{
+            {person.uri} foaf:firstName "{person.first_name}" ;
+                         foaf:lastName "{person.last_name}" .
+        }}
+        """
+        self.service.insert(query)
 
-        from SPARQLWrapper import POST
-
-        query = self.build_prefixes()
-        query += "\n\nINSERT DATA {\n"
-
-        for s, p, o in person.as_triplets():
-            query += f'\t{s} {p} "{o}" .\n'
-
-        query += "}"
-
-        logger.debug(f"SPARQL query:\n{query}")
-
-        self.sparql.setMethod(POST)
-        self.sparql.setQuery(query)
-        response = self.sparql.query().response
-        response = json.loads(response.read().decode("utf-8"))
-        return response
-
-
-if __name__ == "__main__":
-    repo = SPARQLRepository()
-    person_ = Person(uri=":tizio", first_name="Caio", last_name="Sempronio")
-    print(repo.create_person(person_))
+    def retrieve_person(self, resource_id) -> Person:
+        # query = ...
+        # self.service.select(query)
+        raise NotImplementedError
