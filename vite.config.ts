63646873cb52493abd4a9af7072b99cc98af5c14
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2025-12-09 04:46:06 +0100

    Add servers to allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
+  server: {
+    allowedHosts: ['permea.giobber.dev', 'permea.afor.dev', 'localhost'],
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
