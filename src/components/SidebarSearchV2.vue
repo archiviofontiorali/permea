2d8f4b93ebe8612777fbfa1a93e33009e97e65e4
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2025-12-10 18:38:19 +0100

    Add Recipes, Reorganize state for SidebarSearchV2, add methods to add node

--- src/components/SidebarSearchV2.vue
+++ src/components/SidebarSearchV2.vue
@@ -1,56 +1,140 @@
 <script setup lang="ts">
 import { useSPARQLStore, type Binding } from '@/stores/sparql'
+import { useStorageStore, type LinkNode } from '@/stores/storage'
 import { onMounted, reactive, ref } from 'vue'
+import { FaPlusCircle, FaEye } from 'vue-icons-plus/fa'
 
-interface Table {
-  headers: string[]
-  rows: Binding[]
+const RECIPES = [
+  ['All triples', 'SELECT ?s ?v ?o\nWHERE { ?s ?v ?o . }'],
+  [
+    'List items',
+    `SELECT ?s ?title (COUNT(DISTINCT ?v) AS ?properties)\nWHERE {\n\t?s\t?v\t\t\t\t?o;\n\t\tdcterms:title\t?title .\n}\nGROUP BY ?title ?s`,
+  ],
+  ['Single item properties', `SELECT ?property ?value WHERE { permea:uri ?property ?value }`],
+]
+
+interface SearchError {
+  level: 'ERROR' | 'WARNING'
+  value: string
 }
 
-const query = ref<string>('SELECT ?s ?v ?o WHERE { ?s ?v ?o . }')
-const table: Table = reactive({ headers: [], rows: [] })
-const errors: string[] = reactive([])
+interface Search {
+  query?: string
+  results?: {
+    headers: string[]
+    rows: Binding[]
+  }
+  errors: SearchError[]
+}
 
 const sparql = useSPARQLStore()
+const storage = useStorageStore()
 
-onMounted(() => {
-  sparql.updateNamespaces()
-  sparql.updateProperties()
-})
+const actualQuery = ref<string>(RECIPES[0][1])
+const lastSearch: Search = reactive({ errors: [] })
 
-function search(query: string) {
-  errors.splice(0)
+/** Request data using SPARQL query */
+function search(query?: string) {
+  lastSearch.errors.splice(0)
+  sparql
+    .query(query ? query : actualQuery.value)
+    .then((response) => {
+      lastSearch.results = {
+        headers: response.head.vars,
+        rows: response.results.bindings,
+      }
+    })
+    .catch((error) => lastSearch.errors.push({ level: 'ERROR', value: error.response.data }))
+}
+
+function addNode(uri: string) {
+  uri = sparql.namespace(uri)
+
+  if (storage.findNode(uri)) {
+    lastSearch.errors.push({ level: 'ERROR', value: `Node with id: ${uri} already exists` })
+    return
+  }
+  const node: LinkNode = {
+    id: uri,
+    type: 'link',
+    x: 0,
+    y: 0,
+    width: 200,
+    height: 200,
+    url: uri,
+  }
+  storage.createNode(node)
+
+  const query = `SELECT ?property ?value WHERE { ${uri} ?property ?value . }`
   sparql
     .query(query)
     .then((response) => {
-      table.headers = response.head.vars
-      table.rows = response.results.bindings
+      const node = storage.getNode(uri)
+      response.results.bindings.forEach(({ property, value }) => {
+        if (!node.metadata) node.metadata = new Map()
+        node.metadata.set(sparql.namespace(property.value), value)
+      })
     })
-    .catch((error) => errors.push(error.response.data))
+    .catch((error) => lastSearch.errors.push({ level: 'ERROR', value: error.response.data }))
+    .finally(() => console.debug(storage.findNode(uri)))
 }
+
+function showProperties(uri: string) {
+  uri = sparql.namespace(uri)
+  search(`SELECT ?property ?value WHERE { ${uri} ?property ?value }`)
+}
+
+onMounted(() => {
+  // sparql.updateNamespaces()
+  // sparql.updateProperties()
+})
 </script>
 
 <template>
   <aside :class="$attrs.class" class="bg-slate-300">
-    <section id="search-form" class="p-2 grid gap-2 w-full">
-      <textarea name="query" id="query" v-model="query" class="outline-1 min-h-50" />
-      <button class="p-1 outline-1 active:outline-2" @click="search(query)">Search</button>
+    <section id="search-form" class="p-2 grid grid-cols-3 gap-2 w-full">
+      <label for="recipe">Examples: </label>
+      <select name="recipe" id="recipe" class="col-span-2">
+        <option
+          :key="index"
+          :value="name"
+          v-for="([name, recipe], index) of RECIPES"
+          @click="actualQuery = recipe"
+        >
+          {{ name }}
+        </option>
+      </select>
+
+      <textarea
+        name="query"
+        id="query"
+        v-model="actualQuery"
+        class="font-mono min-h-50 col-span-3"
+      />
+
+      <button class="p-1 col-start-2" @click="search()">Search</button>
     </section>
+    <hr class="my-4 border-primary" />
     <section id="search-results" class="w-full overflow-x-auto">
-      <p class="border-2 border-red-800" :key="index" v-for="(e, index) of errors">{{ e }}</p>
-      <table class="table-auto">
+      <p class="border-2 border-red-800" :key="index" v-for="(e, index) of lastSearch.errors">
+        {{ e }}
+      </p>
+      <table class="table-auto" v-if="lastSearch.results">
         <thead>
-          <th :key="h" v-for="h in table.headers">{{ h }}</th>
+          <th :key="h" v-for="h in lastSearch.results.headers">{{ h }}</th>
         </thead>
         <tbody>
-          <tr :key="i" v-for="(row, i) in table.rows" class="_w-full">
-            <td :key="j" v-for="(b, j) in row" class="px-2 text-nowrap">
-              <a
-                class="link block text-nowrap overflow-x-auto max-w-[40ch]"
-                :href="b.value"
-                v-if="b.type === 'uri'"
-                >{{ sparql.namespace(b.value) }}</a
-              >
+          <tr :key="i" v-for="(row, i) in lastSearch.results.rows" class="_w-full">
+            <td :key="j" v-for="(b, j) in row" class="px-2 text-nowrap whitespace-nowrap">
+              <template v-if="b.type === 'uri'">
+                <fa-plus-circle class="inline pr-2" @click="addNode(b.value)" />
+                <fa-eye class="inline pr-2" @click="showProperties(b.value)" />
+
+                <a class="link text-nowrap overflow-x-auto max-w-[45ch]" :href="b.value">
+                  {{ sparql.namespace(b.value) }}
+                </a>
+              </template>
+
               <template v-else>{{ b.value }}</template>
             </td>
           </tr>
